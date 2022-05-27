f155c3b2fb3b2fae53ffea69b78ad18adf1e7a3f
hamitmizrak@gmail.com <hamitmizrak@gmail.com>
2022-05-27 20:51:26 +0300

    Security ==> exclude

--- src/main/java/com/hamitmizrak/TeachcareerTubisadSpringBootApplication.java
+++ src/main/java/com/hamitmizrak/TeachcareerTubisadSpringBootApplication.java
@@ -3,11 +3,19 @@ package com.hamitmizrak;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+//exclude:dahil etmemek
+@SpringBootApplication(exclude = {
+        org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class,
+        org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration.class
+}
+)
 public class TeachcareerTubisadSpringBootApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TeachcareerTubisadSpringBootApplication.class, args);
+
+        // AWT: JOptionPanel set ayarı
+        System.setProperty("java.awt.headless", "false");
     }
 
 }
