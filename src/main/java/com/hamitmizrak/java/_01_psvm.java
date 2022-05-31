b47e733f1003ecb1ae4e3a6ff99267f420117620
hamitmizrak@gmail.com <hamitmizrak@gmail.com>
2022-05-31 18:39:27 +0300

    homework

---
+++ src/main/java/com/hamitmizrak/java/_01_psvm.java
@@ -0,0 +1,34 @@
+package com.hamitmizrak.java;
+
+public class _01_psvm {
+
+    /*psvm ==> TAB*/
+    public static void main(String[] args) {
+        /*sout ==>TAB*/
+        System.out.println("Merhabalar Nasilsin");
+
+        //100 OOP ?
+        //primitive type
+
+        //tam sayılar
+        byte b1 = 44;  //2^0=1 byte ==> 1*8=8bit -1=7  2^7=128  -128<=X<=127
+        short s1 = 66;
+        int i1 = 62;
+        long l1 = 56865156L;
+
+        //binary = 2 (0-1)
+        float f2 = 114.525f;
+        double d2 = 1551.55;
+
+        boolean b3 = 3 > 1;
+        char c3 = '\u5632';
+        System.out.println(c3);
+
+        String s3 = "Merhabalar";
+        System.out.println(s3.toUpperCase());
+
+        //StringBuilder StringTokenizer
+
+        System.out.println(Math.pow(2,5));
+    }
+}
