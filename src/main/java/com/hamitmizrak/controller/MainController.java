38aee1b49aee8de9e944e0026ebd143437d6cdf8
hamitmizrak@gmail.com <hamitmizrak@gmail.com>
2022-06-02 20:32:22 +0300

    Thymeleaf Model

--- src/main/java/com/hamitmizrak/controller/MainController.java
+++ src/main/java/com/hamitmizrak/controller/MainController.java
@@ -4,6 +4,7 @@ import com.hamitmizrak.bean.StudentBean;
 import com.hamitmizrak.dto.StudentDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -24,7 +25,14 @@ public class MainController {
     //http://localhost:8080/thymeleaf1
     @GetMapping("/thymeleaf1")//url
     public String getTymeleaf1(){
-        return "thymeleaf_page"; //html
+        return "thymeleaf_page1"; //html
+    }
+
+    //http://localhost:8080/thymeleaf2
+    @GetMapping("/thymeleaf2")//url
+    public String getTymeleaf2(Model model){
+        model.addAttribute("key_controller","Ben Javadan geldim");
+        return "thymeleaf_page2"; //html
     }
 
 
