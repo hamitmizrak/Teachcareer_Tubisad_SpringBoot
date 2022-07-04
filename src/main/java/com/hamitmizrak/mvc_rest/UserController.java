8d28209aa3ce2cc4d6ad10bea92fabb06d8e6340
hamitmizrak <hamitmizrak@gmail.com>
2022-07-04 18:55:36 +0300

    ResponseEntity<UserDto>

--- src/main/java/com/hamitmizrak/mvc_rest/UserController.java
+++ src/main/java/com/hamitmizrak/mvc_rest/UserController.java
@@ -1,6 +1,9 @@
 package com.hamitmizrak.mvc_rest;
 
 import com.hamitmizrak.business.dto.UserDto;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,14 +14,20 @@ public class UserController {
 
     // http://localhost:8080/user/get/controller
     @GetMapping("user/get/controller")
-    public String getUserController(Model model){
+    public String getUserController(Model model) {
 
         //api url
-        String URL="http://localhost:8080/user/get/Hamit";
-        RestTemplate restTemplate=new RestTemplate();
-        UserDto userDto=  restTemplate.getForObject(URL, UserDto.class);
+        String URL = "http://localhost:8080/user/get/Hamit";
+        RestTemplate restTemplate = new RestTemplate();
 
-        model.addAttribute("user_mvc_rest_key",userDto);
+        //ResponseEntity ==> Kullanmadan
+        //UserDto userDto=  restTemplate.getForObject(URL, UserDto.class);
+
+        //ResponseEntity ==> Kullanarak
+        ResponseEntity<UserDto> responseEntity = restTemplate.exchange(URL, HttpMethod.GET, HttpEntity.EMPTY, UserDto.class);
+        UserDto userDto = responseEntity.getBody();
+
+        model.addAttribute("user_mvc_rest_key", userDto);
         return "user_mvc_rest";
     }
 }
